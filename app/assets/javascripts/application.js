b580e40e117a50c3c01ac17b0fc1ab4bd8ed5bae
sancraz <sancrazm@gmail.com>
2015-08-12 11:54:32 +0300

    Added new header, removed all cssfiles and put one global style.css for all pages.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 
 
 //= require jquery
+//= require bootstrap
 //= require jquery_ujs
 //= require lazyload
 //= require jquery.lazyload.min
---
+++ app/assets/javascripts/backbone/views/authors/header_view.js
@@ -0,0 +1,4 @@
+ArtistGallery.Views.Authors ||= {}
+
+class ArtistGallery.Views.Authors.HeaderView extends Backbone.View
+  template: JST["backbone/templates/authors/header"]
