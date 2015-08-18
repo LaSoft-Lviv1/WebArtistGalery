519c652aa3d09bc14f5e130ca9fa479948cc7a2d
Vasyl <varkholyak@gmail.com>
2015-08-18 17:11:44 +0300

    Added small changes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,13 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
 
-  root to: "application#index"
+  root to: 'application#index'
   # resources :posts
   # resources :upload_image
   #s
   resources :authors
-
   resources :city
-
   resources :art_items
-
   resources :categories
 
   # resources :users/sessions, only: [ :create, :destroy ]
