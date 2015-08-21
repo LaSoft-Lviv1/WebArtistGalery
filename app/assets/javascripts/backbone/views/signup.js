f083b0418212ff56692834ab7cb92665e8a51068
VanOk1205 <vanvan@bigmir.net>
2015-08-21 11:40:41 +0300

    Fixed modal dialog. Only login during first click on login-registratoin link.

--- app/assets/javascripts/backbone/views/signup.js
+++ app/assets/javascripts/backbone/views/signup.js
@@ -1,13 +1,32 @@
 var SignupView = Backbone.View.extend ({
 	template: JST["backbone/templates/signup"],
 
+    events: {
+        'click button.artist':'artistSignup',
+        'click button#customer': 'customerSignup',
+    },
+
 	initialize: function () {
 	this.render();
-},
+    },
 
-	render: function () {
+    artistSignup: function (e) {
+        e.preventDefault();
+        //$('.modal-content').html( this.template() );
+        alert('artist');
+        return this;
+    },
+
+    customerSignup: function (e) {
+        e.preventDefault();
+        //$('.modal-content').html( this.template() );
+        alert('customer');
+        return this;
+    },
+
+    render: function () {
 	$('.modal-content').html( this.template() );
 
 	return this;
-}
+    }
 });
