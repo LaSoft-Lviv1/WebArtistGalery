fa6ac4e675c0fbd0fcb78a8849d0e203f5839ccb
Igor Rud <ihor_rud@ymail.com>
2015-09-06 21:05:07 +0300

    detailed-view finished

--- public/frontend/js/detailed-item-view.js
+++ public/frontend/js/detailed-item-view.js
@@ -9,6 +9,16 @@ $('[data-target="#modal-fullsize-img"]').on('click', function(){
     $('#modal-fullsize-img-title').text(title);
     $('.modal-body').html('<img class="img-responsive-height" src="'+ imgSrc + '" alt="' + title + '">');
 })
-// $('#myModal').on('show.bs.modal', function () {
-// $('.modal-content').css('height',$( window ).height()*0.8);
-// });
+
+function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+});
