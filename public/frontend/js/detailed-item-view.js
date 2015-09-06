368dc2e51fc89a11a211df19490e61fd3c34933d
Igor Rud <ihor_rud@ymail.com>
2015-09-06 20:21:27 +0300

    control commit

--- public/frontend/js/detailed-item-view.js
+++ public/frontend/js/detailed-item-view.js
@@ -7,7 +7,7 @@ $('[data-target="#modal-fullsize-img"]').on('click', function(){
     var title = $('.selected-image').attr("title");
     var imgSrc = $('.selected-image').attr("src");
     $('#modal-fullsize-img-title').text(title);
-    $('.modal-body').html('<img src="'+ imgSrc + '" alt="' + title + '">');
+    $('.modal-body').html('<img class="img-responsive-height" src="'+ imgSrc + '" alt="' + title + '">');
 })
 // $('#myModal').on('show.bs.modal', function () {
 // $('.modal-content').css('height',$( window ).height()*0.8);
