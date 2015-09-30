e072236d00af71baa72b601504738979ca8ad96c
VanOk1205 <vanvan@bigmir.net>
2015-09-30 16:21:09 +0300

    Fixing bags in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,12 +81,14 @@ Rails.application.configure do
 
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
-		address: "smtp.gmail.com",
-		port: 587,
-		domain: "gmail.com",
-		authentication: "plain",
-		enable_starttls_auto: true,
-		user_name: "van.test.mailer@gmail.com",
-		password: "Xs12051987"
+			address: "smtp.gmail.com",
+			port: 587,
+			domain: "gmail.com",
+			authentication: "plain",
+			enable_starttls_auto: true,
+			user_name: "van.test.mailer@gmail.com",
+			password: "Xs12051987"
 	}
+	# config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+	config.action_mailer.default_url_options = { :host => 'radiant-ridge-8447.herokuapp.com' }
 end
