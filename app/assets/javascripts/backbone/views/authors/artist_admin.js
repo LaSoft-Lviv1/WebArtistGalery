6a2e64d9a3fcfc08d875813b2c1432340a012028
Art <vovapaskiv@gmail.com>
2015-10-06 13:56:02 +0300

    add Unlock

--- app/assets/javascripts/backbone/views/authors/artist_admin.js
+++ app/assets/javascripts/backbone/views/authors/artist_admin.js
@@ -3,12 +3,12 @@ var ArtistAdminView = Backbone.View.extend ({
 	template: JST['backbone/templates/authors/new_artist_page'],
 
 	initialize: function () {
-
 		this.render();
+		console.log(this.model);
 	},
 
 	render: function () {
-		$('#content').html( this.template() );
+		$('#content').html( this.template(  ) );
 		return this;
 	}
 
